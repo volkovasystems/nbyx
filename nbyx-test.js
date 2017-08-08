7e8fcea690ba742bb95cb5e9c8e8440812071624
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 15:08:20 +0800

    Updated test.

--- nbyx-test.js
+++ nbyx-test.js
@@ -7,12 +7,12 @@ assert.deepEqual( nbyx( [ 1, 2, 3, 4, 5, 6, 7, 8 ], 2 ), [
 	[ 3, 4 ],
 	[ 5, 6 ],
 	[ 7, 8 ]
-], "should be deeply equal" );
+], "should be equal to [ [ 1, 2 ], [ 3, 4 ], [ 5, 6 ], [ 7, 8 ] ]" );
 
 assert.deepEqual( nbyx( [ 1, 2, 3, 4, 5, 6, 7, 8 ], 3 ), [
 	[ 1, 2, 3 ],
 	[ 4, 5, 6 ],
 	[ 7, 8 ]
-], "should be deeply equal" );
+], "should be equal to [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8 ] ]" );
 
 console.log( "ok" );
